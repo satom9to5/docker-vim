9a7569f63e641349830620c584fb8209640d0eab
matsuo.satoshi <matsuo.satoshi@vega-c.com>
2019-01-28 02:09:03 +0000

    fix run.sh

--- ruby/run.sh
+++ ruby/run.sh
@@ -3,4 +3,4 @@
 DOCKER_IMAGE=alpine-vim-ruby
 MOUNT_DIR=${1:-${HOME}}
 
-docker run -it --rm -v ${MOUNT_DIR}:/vim ${DOCKER_IMAGE} 
+docker run -it --rm -v ${MOUNT_DIR}:/vim -e COLUMNS=$(tput cols) -e LINES=$(tput lines) ${DOCKER_IMAGE} 
